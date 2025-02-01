7decb5d3c025a17c0a27694859703bea6fca1519
Daniel Thalmann <daniel@thalmann.li>
2025-02-01 14:25:06 +0100

    rename to php

---
+++ src/HttpAcceptLanguage.php
@@ -0,0 +1,70 @@
+<?php
+
+class HttpAcceptLanguage
+{
+    protected $acceptedLanguages = [];
+    function __construct(?string $httpAcceptLanguage)
+    {
+        if ($httpAcceptLanguage . '' != '') {
+            foreach(explode(',', $httpAcceptLanguage) as $langAndQuality) {
+                $langs = explode (';', $langAndQuality);
+                if (count($langs) == 1) {
+                    $langs[1] = 'q=1.0';
+                }
+
+                $this->acceptedLanguages[] = new HttpLanguage($langs[0], substr($langs[1], 2));
+            }
+            uasort($this->acceptedLanguages, 'languageCompare');
+        }
+    }
+
+    function getLanguage(array $testLanguages, ?string $fallback = null)
+    {
+        foreach($testLanguages as $testLanguage) {
+            foreach($this->acceptedLanguages as $acceptedLanguage) {
+                if (($acceptedLanguage->language) == ($testLanguage)) {
+                    return $testLanguage;
+                }
+            }
+        }
+        return $fallback;
+    }
+
+    function __tostring()
+    {
+        return implode(',', $this->acceptedLanguages);
+    }   
+}
+
+function languageCompare($a, $b) {
+    if ($a->quality == $b->quality) {
+        return 0;
+    }
+    return ($a->quality > $b->quality) ? -1 : 1;
+}
+
+class HttpLanguage
+{
+    public $language;
+
+    public $country = null;
+
+    public $quality;
+
+    function __construct(?string $language = "", float $quality = 1.0)
+    {
+        $langCountry = explode('-', $language);
+
+        $this->language = $langCountry[0];
+        if(count($langCountry) > 1) {
+            $this->country = $langCountry[1];
+        }
+        $this->quality = $quality;
+    }
+
+    function __tostring()
+    {
+        return $this->language . (($this->country) ? '-' . $this->country : '') . ';q=' . round($this->quality, 1);
+    }
+
+}
---
+++ src/index.php
@@ -0,0 +1,8 @@
+<?php
+    include ("HttpAcceptLanguage.php");
+    
+    $l = new HttpAcceptLanguage($_SERVER["HTTP_ACCEPT_LANGUAGE"]);
+    $path = $l->getLanguage(['fr', 'de'], 'fr');
+
+    header('Location: /' . $path,true,301);
+?>
