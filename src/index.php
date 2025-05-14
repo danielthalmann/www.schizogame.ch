33c5b37af8307bb617f9d17399400fd67cca6b81
Daniel Thalmann <daniel@thalmann.li>
2025-05-14 11:25:53 +0200

    fix language selection

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
     include ("HttpAcceptLanguage.php");
     
-    $l = new HttpAcceptLanguage($_SERVER["HTTP_ACCEPT_LANGUAGE"]);
+    $l = new HttpAcceptLanguage($_SERVER["HTTP_ACCEPT_LANGUAGE"] ?? '');
     $path = $l->getLanguage(['fr', 'de'], 'fr');
 
     header('Location: /' . $path,true,301);
