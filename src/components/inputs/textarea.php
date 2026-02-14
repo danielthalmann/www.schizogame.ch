9c219185bad3ad5415c252c2a18f8a6536e7b169
Daniel Thalmann <daniel@thalmann.li>
2026-02-14 17:59:18 +0100

    add new formfield

---
+++ src/components/inputs/textarea.php
@@ -0,0 +1,18 @@
+<div class="mb-3">
+    <?php if (isset($label)) : ?>
+        <label class="mb-1 block font-bold" for="<?= $name ?? 'name' ?>_id">
+            <?= $label ?>
+            <?php if (isset($mandatory) && $mandatory) : ?>
+                <?= $mandatory_label ?? '*' ?>
+            <?php endif; ?>
+        </label>
+    <?php endif; ?>
+    <textarea class="block focus-visible:outline-gray-600 focus-visible:outline-2 hover:outline-1 hover:outline-gray-600 w-full border-1 rounded-sm p-2" 
+        name="<?= $name ?? 'name' ?>"
+        id="<?= $name ?? 'name' ?>_id"
+        type="textarea" 
+        <?php if (isset($mandatory) && $mandatory) : ?>
+        mandatory="mandatory"
+        <?php endif; ?>
+    ><?= $value ?? '' ?></textarea>
+</div>
