51889f718cb5a5fbf1dc2ede5bda16126d073479
Daniel Thalmann <daniel@thalmann.li>
2026-02-16 13:55:43 +0100

    fix namespace

--- src/index.php
+++ src/index.php
@@ -1,9 +1,7 @@
 <?php
     include ("HttpAcceptLanguage.php");
     
-    use HttpAcceptLanguage;
-    
-    $l = new HttpAcceptLanguage($_SERVER["HTTP_ACCEPT_LANGUAGE"] ?? '');
+    $l = new \HttpAcceptLanguage($_SERVER["HTTP_ACCEPT_LANGUAGE"] ?? '');
     $path = $l->getLanguage(['fr', 'de'], 'fr');
 
     header('Location: /' . $path,true,301);
